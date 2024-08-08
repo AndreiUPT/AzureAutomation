8692017a40aed58b941758e54ee66416c5919bb7
AndreiUPT <106157763+AndreiUPT@users.noreply.github.com>
2024-08-08 11:42:15 +0300

    Added parameters and AZ account connecting Try-Catch block in CopySnapshotToOtherRegion.ps1

--- CopySnapshotToOtherRegion.ps1
+++ CopySnapshotToOtherRegion.ps1
@@ -1,1 +1,14 @@
+param (
+    [String] $sourceResourceGroup = 'RG-AndreiUPT',
+    [String] $subscriptionId = 'a24b4eab-e1ce-49f2-b025-cf22b0e48aa3',
+    [String] $targetRegion = "westeurope"
+)
 
+try {
+    # Authenticate and select subscription
+    Connect-AzAccount -Identity
+    Select-AzSubscription -SubscriptionId $subscriptionId
+} catch {
+    Write-Error -Message "Failed to authenticate using Managed Identity. Error: $_"
+    throw $_
+}
