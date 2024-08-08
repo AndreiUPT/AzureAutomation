898decce2fcecdbae2641506d1a05e4ea89ca91b
AndreiUPT <106157763+AndreiUPT@users.noreply.github.com>
2024-08-08 11:14:14 +0300

    Added the parameters in CreateFullVMsBackup.ps1

--- CreateFullVMsBackup.ps1
+++ CreateFullVMsBackup.ps1
@@ -1,1 +1,6 @@
-
+param (
+    [String] $resourceGroupName = 'RG-AndreiUPT',
+    [String] $snapshotNamePrefix = 'snapshot-1',  #prefix name convention for full snapshots
+    [String] $subscriptionId = 'a24b4eab-e1ce-49f2-b025-cf22b0e48aa3',
+    [String] $location = 'north europe'  
+)
