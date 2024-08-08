5c1fc797117d0ab60f78bf2ab093c15fddb23422
AndreiUPT <106157763+AndreiUPT@users.noreply.github.com>
2024-08-08 11:28:50 +0300

    Added parameters and Try-Catch block to connect to Az account in Incrementalsnapshot.ps1

--- Incrementalsnapshot.ps1
+++ Incrementalsnapshot.ps1
@@ -1,1 +1,13 @@
+param (
+    [String] $resourceGroup = 'RG-AndreiUPT',           
+    [String] $subscriptionId = 'a24b4eab-e1ce-49f2-b025-cf22b0e48aa3',
+    [String] $region = 'northeurope'           
+)
 
+try {
+    Connect-AzAccount -Identity
+    Select-AzSubscription -SubscriptionId $subscriptionId
+} catch {
+    Write-Error -Message "Failed to authenticate using Managed Identity. Error: $_"
+    throw $_
+}
