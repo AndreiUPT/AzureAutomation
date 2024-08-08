f8569720711d4b0d8e547102ab6c03fde615e2d5
AndreiUPT <106157763+AndreiUPT@users.noreply.github.com>
2024-08-08 11:15:48 +0300

    Added Try-Catch block to connect to AZ account in CreateFullVMsBackup.ps1

--- CreateFullVMsBackup.ps1
+++ CreateFullVMsBackup.ps1
@@ -4,3 +4,11 @@ param (
     [String] $subscriptionId = 'a24b4eab-e1ce-49f2-b025-cf22b0e48aa3',
     [String] $location = 'north europe'  
 )
+
+try {
+    Connect-AzAccount -Identity
+    Select-AzSubscription -SubscriptionId $subscriptionId
+} catch {
+    Write-Error -Message "Failed to authenticate using Managed Identity. Error: $_"
+    throw $_
+}
