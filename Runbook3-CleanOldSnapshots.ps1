daad1a07e7548c421185b88b3f8fb5f4a08209cc
AndreiUPT <106157763+AndreiUPT@users.noreply.github.com>
2024-08-20 17:35:20 +0300

    Added parameters and Login to AZ account in Runbook3-CleanOldSnapshots.ps1

--- Runbook3-CleanOldSnapshots.ps1
+++ Runbook3-CleanOldSnapshots.ps1
@@ -1,1 +1,13 @@
+param (
+    [String] $resourceGroupName = 'RG-AndreiUPT',
+    [String] $subscriptionId = 'a24b4eab-e1ce-49f2-b025-cf22b0e48aa3',
+    [Int] $retentionDays = 1
+)
 
+try {
+    Connect-AzAccount -Identity
+    Select-AzSubscription -SubscriptionId $subscriptionId
+} catch {
+    Write-Error -Message "Failed to authenticate using Managed Identity. Error: $_"
+    throw $_
+}
