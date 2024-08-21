6d88096974f4e02c6a231fbaee1ab25abbff637d
AndreiUPT <106157763+AndreiUPT@users.noreply.github.com>
2024-08-21 17:40:50 +0300

    Added params + AZ Auth in DeleteOldManagedBackupDisk.ps1

--- DeleteOldManagedBackupDisk.ps1
+++ DeleteOldManagedBackupDisk.ps1
@@ -1,1 +1,15 @@
+param (
+    [String] $resourceGroupName = 'RG-AndreiUPT',
+    [String] $subscriptionId = 'a24b4eab-e1ce-49f2-b025-cf22b0e48aa3',
+    [String] $diskNameSubstring = 'backup',
+    [Int] $retentionDays = 7
+)
 
+try {
+    Connect-AzAccount -Identity
+    Select-AzSubscription -SubscriptionId $subscriptionId
+    Write-Output "Authenticated successfully."
+} catch {
+    Write-Error -Message "Failed to authenticate using Managed Identity. Error: $_"
+    throw $_
+}
