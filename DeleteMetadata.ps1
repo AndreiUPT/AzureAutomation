0061edf74f1b5689d348a8713916690357c4419b
AndreiUPT <106157763+AndreiUPT@users.noreply.github.com>
2024-08-21 17:21:00 +0300

    Added param + AZ Auth block in DeleteMetadata.ps1

--- DeleteMetadata.ps1
+++ DeleteMetadata.ps1
@@ -1,1 +1,15 @@
+param (
+    [String] $vaultName = 'aio-kv-upt',
+    [String] $subscriptionId = 'a24b4eab-e1ce-49f2-b025-cf22b0e48aa3',
+    [Int] $retentionDays = 2
+)
+
+try {
+    Connect-AzAccount -Identity
+    Select-AzSubscription -SubscriptionId $subscriptionId
+    Write-Output "Authenticated successfully."
+} catch {
+    Write-Error -Message "Failed to authenticate using Managed Identity. Error: $_"
+    throw $_
+}
 
