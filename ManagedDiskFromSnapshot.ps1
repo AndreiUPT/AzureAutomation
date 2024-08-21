151370e4f46611f5062f9d1c54add98c9e949aed
AndreiUPT <106157763+AndreiUPT@users.noreply.github.com>
2024-08-21 17:26:58 +0300

    Added params + AZ Auth in  ManagedDiskFromSnapshot.ps1

--- ManagedDiskFromSnapshot.ps1
+++ ManagedDiskFromSnapshot.ps1
@@ -1,1 +1,17 @@
+param (
+    [String] $resourceGroupName = 'RG-AndreiUPT',
+    [String] $diskNamePrefix = 'backupDisk-',
+    [Int] $diskSizeGB = 128,
+    [String] $location = 'northeurope',
+    [String] $subscriptionId = 'a24b4eab-e1ce-49f2-b025-cf22b0e48aa3',
+    [String] $snapshotPrefix = 'snapshot-1'
+)
 
+try {
+    Connect-AzAccount -Identity
+    Select-AzSubscription -SubscriptionId $subscriptionId
+    Write-Output "Authenticated successfully."
+} catch {
+    Write-Error -Message "Failed to authenticate using Managed Identity. Error: $_"
+    throw $_
+}
