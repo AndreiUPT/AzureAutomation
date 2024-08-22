b56418f7590fce36c848a4af15adfaae89f75e9b
AndreiUPT <106157763+AndreiUPT@users.noreply.github.com>
2024-08-22 16:20:01 +0300

    Added the function to register the runbook to schedule in ScheduleCreateFullVMsBackup.ps1

--- ScheduleCreateFullVMsBackup.ps1
+++ ScheduleCreateFullVMsBackup.ps1
@@ -3,3 +3,13 @@ param (
     [String] $automationAccountName = 'AndreiUPTAutomation',
     [String] $subscriptionId = 'a24b4eab-e1ce-49f2-b025-cf22b0e48aa3'
 )
+
+function Register-RunbookWithSchedule {
+    param (
+        [String] $runbookName,
+        [String] $scheduleName,
+        [String] $prefix,
+        [String] $intervalType,
+        [Int] $interval,
+        [Datetime] $startTime
+    )
