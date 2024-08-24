5087c87416ff994a320b1f697f6f547f359a3eef
AndreiUPT <106157763+AndreiUPT@users.noreply.github.com>
2024-08-24 16:38:54 +0300

    Function to register runbook with schedule in ScheduleDeleteOldManagedBackupDisk.ps1

--- ScheduleDeleteOldManagedBackupDisk.ps1
+++ ScheduleDeleteOldManagedBackupDisk.ps1
@@ -4,3 +4,12 @@ param (
     [String] $subscriptionId = 'a24b4eab-e1ce-49f2-b025-cf22b0e48aa3'
 )
 
+function Register-RunbookWithSchedule {
+    param (
+        [String] $runbookName,
+        [String] $scheduleName,
+        [String] $intervalType,
+        [Int] $interval,
+        [Datetime] $startTime,
+        [Hashtable] $runbookParameters
+    )
