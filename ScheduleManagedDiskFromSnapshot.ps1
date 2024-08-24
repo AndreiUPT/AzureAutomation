c6f64ed55e77252e845d371f65eb24571c64df04
AndreiUPT <106157763+AndreiUPT@users.noreply.github.com>
2024-08-24 16:32:38 +0300

    Params added in ScheduleManagedDiskFromSnapshot.ps1

--- ScheduleManagedDiskFromSnapshot.ps1
+++ ScheduleManagedDiskFromSnapshot.ps1
@@ -1,1 +1,5 @@
-
+param (
+    [String] $resourceGroupName = 'RG-AndreiUPT',
+    [String] $automationAccountName = 'AndreiUPTAutomation',
+    [String] $subscriptionId = 'a24b4eab-e1ce-49f2-b025-cf22b0e48aa3'
+)
