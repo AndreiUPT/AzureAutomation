bfe362ca79debab2aeb2983033d8bcaa6b06e7a5
AndreiUPT <106157763+AndreiUPT@users.noreply.github.com>
2024-08-24 16:22:43 +0300

    Function to register the runbook into the schedule in ScheduleStoreMetadata.ps1

--- ScheduleStoreMetadata.ps1
+++ ScheduleStoreMetadata.ps1
@@ -4,3 +4,13 @@ param (
     [String] $subscriptionId = 'a24b4eab-e1ce-49f2-b025-cf22b0e48aa3',
     [String] $vaultName = 'aio-kv-upt'
 )
+
+function Register-RunbookWithSchedule {
+    param (
+        [String] $runbookName,
+        [String] $scheduleName,
+        [String] $intervalType,
+        [Int] $interval,
+        [Datetime] $startTime,
+        [Hashtable] $runbookParameters
+    )
