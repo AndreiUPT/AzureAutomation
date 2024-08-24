38a7aa700c59ed5379de8be75e9474136f914af9
AndreiUPT <106157763+AndreiUPT@users.noreply.github.com>
2024-08-24 16:21:56 +0300

    Added params in ScheduleStoreMetadata.ps1

--- ScheduleStoreMetadata.ps1
+++ ScheduleStoreMetadata.ps1
@@ -1,1 +1,6 @@
-
+param (
+    [String] $resourceGroupName = 'RG-AndreiUPT',
+    [String] $automationAccountName = 'AndreiUPTAutomation',
+    [String] $subscriptionId = 'a24b4eab-e1ce-49f2-b025-cf22b0e48aa3',
+    [String] $vaultName = 'aio-kv-upt'
+)
